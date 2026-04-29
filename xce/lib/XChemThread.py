10c284f62b19ec53f4144626986136fe97ad2292
smythss <smyth.s@wehi.edu.au>
2026-04-29 12:50:35 +1000

    fixing slurm to use sbatch to bypass

--- xce/lib/XChemThread.py
+++ xce/lib/XChemThread.py
@@ -1778,16 +1778,39 @@ class run_dimple_on_all_autoprocessing_files_new(QtCore.QThread):
             + "[ -f /etc/profile.d/modules.sh ] && . /etc/profile.d/modules.sh || true\n"
             + "./xce_{!s}{!s}_$SLURM_ARRAY_TASK_ID.sh\n".format(self.pipeline, twin)
         )
-        f = open("{!s}{!s}_master.sh".format(self.pipeline, twin), "w")
+        master_script = "{!s}{!s}_master.sh".format(self.pipeline, twin)
+        f = open(master_script, "w")
         f.write(Cmds)
         f.close()
-        slurm.submit_cluster_job(
-            "xce_{!s}{!s}_master".format(self.pipeline, twin),
-            "{!s}{!s}_master.sh".format(self.pipeline, twin),
-            self.xce_logfile,
-            self.slurm_token,
-            array="1-{!s}".format(self.n - 1),
-        )
+        job_name = "xce_{!s}{!s}_master".format(self.pipeline, twin)
+        array_spec = "1-{!s}".format(self.n - 1)
+        if not os.environ.get("CLUSTER_HOST", ""):
+            import subprocess
+            partition = os.environ.get("CLUSTER_PARTITION", "regular")
+            sbatch_cmd = [
+                "sbatch",
+                "--partition", partition,
+                "--job-name", job_name,
+                "--array", array_spec,
+                "--output", os.path.join(self.ccp4_scratch_directory, job_name + ".stdout"),
+                "--error",  os.path.join(self.ccp4_scratch_directory, job_name + ".stderr"),
+                master_script,
+            ]
+            self.Logfile.insert("Submitting via sbatch: " + " ".join(sbatch_cmd))
+            try:
+                result = subprocess.check_output(sbatch_cmd, stderr=subprocess.STDOUT)
+                self.Logfile.insert("sbatch response: " + result.strip())
+            except subprocess.CalledProcessError as exc:
+                self.Logfile.insert("sbatch failed: " + exc.output)
+                raise
+        else:
+            slurm.submit_cluster_job(
+                job_name,
+                master_script,
+                self.xce_logfile,
+                self.slurm_token,
+                array=array_spec,
+            )
 
 
 class remove_selected_dimple_files(QtCore.QThread):
